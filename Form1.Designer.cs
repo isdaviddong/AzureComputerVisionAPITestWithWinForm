79a7b1fc403dbc0de25f460e62a7271db8bb01fc
David@SurfacePro4 <isdaviddong@gmail.com>
2017-08-26 10:42:59 +0800

    v1

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ComputerVisionAPI_Cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button2 = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Calibri", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button2.Location = new System.Drawing.Point(116, 12);
+            this.Button2.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(168, 40);
+            this.Button2.TabIndex = 7;
+            this.Button2.Text = "人臉辨識";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox1.Font = new System.Drawing.Font("新細明體", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TextBox1.Location = new System.Drawing.Point(10, 340);
+            this.TextBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(628, 122);
+            this.TextBox1.TabIndex = 6;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBox1.Location = new System.Drawing.Point(10, 63);
+            this.PictureBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(628, 263);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 5;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Calibri", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(10, 12);
+            this.Button1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(93, 40);
+            this.Button1.TabIndex = 4;
+            this.Button1.Text = "OCR";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 473);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace ComputerVisionAPI_Cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+
+            if (d.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            string filePath = d.FileName;
+            //OCR OcrResults
+            OcrResults OcrResults = default(OcrResults);
+            //建立VisionServiceClient
+            var visionClient = new Microsoft.ProjectOxford.Vision.VisionServiceClient(
+               "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!換成你的key!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ",
+                "https://southeastasia.api.cognitive.microsoft.com/vision/v1.0");
+            using (var fs  = new FileStream(filePath, FileMode.Open))
+            {
+                //  this.textBox.Text = "辨識中...";
+                //以繁體中文辨識
+                //  this.textBox.Text = "";
+                OcrResults = visionClient.RecognizeTextAsync(fs, LanguageCodes.AutoDetect).Result;
+            }
+
+            string result = "";
+            //抓取每一區塊的辨識結果
+            foreach (var Region in OcrResults.Regions)
+            {
+                //抓取每一行
+                foreach (var line_loopVariable in Region.Lines)
+                {
+                   var line = line_loopVariable;
+                    dynamic aline = "";
+                    //抓取每一個字
+                    foreach (var Word_loopVariable in line.Words)
+                    {
+                      var  Word = Word_loopVariable;
+                        //顯示辨識結果
+                        aline += Word.Text;
+                    }
+
+                    //加換行
+                    result += aline + "\n";
+                }
+            }
+
+            this.TextBox1.Text = result;
+
+            //load picture
+            this.PictureBox1.Image = Image.FromFile(filePath);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog d = new OpenFileDialog();
+
+            if (d.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            string filePath = d.FileName;
+
+            //取得原始檔案讀入BPM
+            var fs2 = new FileStream(filePath, FileMode.Open);
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(fs2);
+            Graphics g = Graphics.FromImage(bmp);
+            fs2.Close();
+
+            //OCR OcrResults
+            AnalysisResult AnalysisResult = default(AnalysisResult);
+
+            //建立VisionServiceClient
+            var visionClient = new Microsoft.ProjectOxford.Vision.VisionServiceClient(
+                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!換成你的key!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ",
+                "https://southeastasia.api.cognitive.microsoft.com/vision/v1.0");
+            using (var fs  = new FileStream(filePath, FileMode.Open))
+            {
+                //  this.textBox.Text = "辨識中...";
+                //以繁體中文辨識
+                //  this.textBox.Text = "";
+                var t = new List<VisualFeature>();
+                t.Add(VisualFeature.Faces);
+                t.Add(VisualFeature.Description);
+                AnalysisResult = visionClient.AnalyzeImageAsync(fs, t).Result;
+            }
+
+            int isM = 0;
+            int isF = 0;
+            int resizeFactor = 1;
+
+            string result = "";
+            //抓取每一區塊的辨識結果
+            foreach (var item_loopVariable in AnalysisResult.Faces)
+            {
+             var   item = item_loopVariable;
+                var faceRect = item.FaceRectangle;
+
+                //畫框
+                g.DrawRectangle(
+                    new Pen(Brushes.Red, 10), 
+                    new System.Drawing.Rectangle((int)faceRect.Left * resizeFactor,
+                    (int)faceRect.Top * resizeFactor, (int)faceRect.Width * resizeFactor, (int)faceRect.Height * resizeFactor));
+                //顯示年紀
+                var age = 0;
+                if (item.Gender.StartsWith("F"))
+                {
+                    age = item.Age - 2;
+                }
+                else
+                {
+                    age = item.Age;
+                }
+                g.DrawString(age.ToString(), new Font("Arial", 16), new SolidBrush(System.Drawing.Color.Black), faceRect.Left * resizeFactor + 3, faceRect.Top * resizeFactor + 3);
+                //紀錄性別
+                if (item.Gender.StartsWith("M"))
+                {
+                    isM += 1;
+                }
+                else
+                {
+                    isF += 1;
+                }
+            }
+
+            var PictureDescription = AnalysisResult.Description.Captions[0].Text;
+            //如果update了照片，則顯示新圖
+            if (AnalysisResult.Faces.Count() > 0)
+            {
+                PictureDescription += String.Format("找到{0}張臉, {1}男 {2}女", AnalysisResult.Faces.Count(), isM, isF);
+                string filename = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(filePath);
+                bmp.Save(filename);
+                //load picture
+                this.PictureBox1.Image = Image.FromFile(filename);
+            }
+
+            this.TextBox1.Text = PictureDescription;
+        }
+    }
+}
